f4f76d2efdefa800d55d21a5c579511a5bac8dc6
aptu <asyatsai@gmail.com>
2023-07-15 18:45:03 -0700

    Test db gets populated from current project's data bootstrapping class

--- src/test/java/com/springcourse/dbProjectSpringBoot/SpringBootJPATestSplice.java
+++ src/test/java/com/springcourse/dbProjectSpringBoot/SpringBootJPATestSplice.java
@@ -8,12 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
+@ComponentScan(basePackages = {"com.springcourse.dbProjectSpringBoot"})
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SpringBootJPATestSplice {
     @Autowired
@@ -24,7 +26,7 @@ public class SpringBootJPATestSplice {
     @Commit
     void testJpaTestSplice() {
         long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(0);
+        assertThat(countBefore).isEqualTo(2);
         bookRepository.save(new Book("My book", "3756", "Self"));
         long countAfter = bookRepository.count();
         assertThat(countBefore).isLessThan(countAfter);
@@ -34,6 +36,6 @@ public class SpringBootJPATestSplice {
     @Order(2)
     void testJpaSpliceTransaction() {
         long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(1);
+        assertThat(countBefore).isEqualTo(3);
     }
 }
